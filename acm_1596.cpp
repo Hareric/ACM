32d331a44d457f7e3200424ec0c59abd286faf22
Eric Chan <571199232@qq.com>
2016-06-27 22:49:03 +0800

    分治法 线性选择

--- acm_1596.cpp
+++ acm_1596.cpp
@@ -1,3 +1,25 @@
+/*
+线性时间选择
+Time Limit: 1000MS	Memory Limit: 65536K
+Total Submission(s): 119	Accepted: 47
+Description
+已知n个数，找出排序后第k小的数。要求平均时间复杂度为O(n)
+
+Input
+第一行两个数字n和k，表示找出n个数字中的第k小的数
+第二行包含n个数字，以空格分隔
+Output
+第k小的数的数值
+Sample Input
+6 2
+5 2 1 3 6 4
+Sample Output
+2
+
+解题思路
+    使用快排的思想进行选择
+*/
+
 # include <iostream>
 # include <vector>
 using namespace std;
