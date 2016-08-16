c359153a00819517a99a3f9fb1bbd98c27762380
Eric Chan <hareric@qq.com>
2016-08-16 18:44:58 +0800

    Update hanoi.cpp

--- hanoi.cpp
+++ hanoi.cpp
@@ -10,12 +10,12 @@ void hanoi(char a, char b, char c, int n)
 	}
 	else
 	{
-		hanoi(a, c, b, n-1);
-		cout << a << '>' << c << endl;
-		hanoi(b, a, c, n-1);
+		hanoi(a, c, b, n-1);  // 先将a柱上的n-1个板移至b柱上
+		cout << a << '>' << c << endl;  // 再将a柱上的最后一个板移动至c柱上
+		hanoi(b, a, c, n-1);  // 最后将b柱剩余的n-1个板移动至c柱上
 	}
 } 
 int main()
 {
-	hanoi('a', 'b', 'c', 10);
+	hanoi('A', 'B', 'C', 10);
 }
